9e84491c6768ea4a0f9ed45b03dc0fabcce5d6a8
miyakey <miyasaviola@gmail.com>
2017-12-12 19:13:40 +0000

    コマンドライン引数確認

--- src/main.go
+++ src/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Print("hello, golang.")
+	f := flag.Int("flag1", 0, "flag 1")
+	flag.Parse()
+
+	fmt.Println("os.Args: ", os.Args)
+	fmt.Println("compare")
+	fmt.Println("flag.Args: ", flag.Args())
+	if *f == 100 {
+		fmt.Println("Hello")
+	}
 }
