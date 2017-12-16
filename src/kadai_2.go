c28535a4e47610bdf49f271ecc30edd8ebd191c0
miyakey <miyasaviola@gmail.com>
2017-12-16 22:23:48 +0000

    素因数

--- src/kadai_2.go
+++ src/kadai_2.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 )
 
+// メイン関数
+// 期待するコマンドライン引数：「./kadai_1 28」の様な形式
 func main() {
 
 	var i int
@@ -17,10 +19,10 @@ func main() {
 
 		fmt.Println("引数:", i)
 		// 約数の足し算結果
-		ans, err := ProcDivisorAdd(i)
+		ans, err := ProcFactorNumber(i)
 
 		if ans > 0 {
-			fmt.Println("約数の合計:", ans)
+			fmt.Println("素因数分解の回答:", ans)
 		} else {
 			fmt.Println("エラー:", err)
 		}
@@ -29,23 +31,33 @@ func main() {
 	}
 }
 
-// 約数の足し算処理
+// 素因数分解処理
 // x:正の自然数
 // result:xの約数の足し算結果（エラー時:0）
 // errObj:エラー型(エラーメッセージ用)
-func ProcDivisorAdd(x int) (result int, errObj error) {
+func ProcFactorNumber(x int) (result int, errObj error) {
 	result = 0
 
 	// 引数チェック
 	if x > 0 {
-		n := 0
-		for n < x {
-			// 約数チェック
-			if x%(n+1) == 0 {
-				result += (n + 1)
-			}
-			n++
-		}
+       i := 2;
+       tmp := x;
+
+        for (i * i) <= x{
+            if tmp % i == 0{
+                tmp /= i
+                fmt.Println("回答:", i)
+                //fmt.Println("tmp:", tmp)
+                ProcFactorNumber(tmp)
+                break;
+            }else{
+                i++
+            }
+        }
+        
+        fmt.Println("終わり回答:", tmp)
+   
+        
 	} else {
 		errObj = errors.New("正の自然数ではありません")
 	}
